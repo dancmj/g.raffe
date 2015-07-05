efee8a330989d86b7bd2b0f7781ca82a741ab1e6
dancmj <daniel.c82@gmail.com>
2015-07-05 15:16:06 -0700

    Created basic routing for home page. Added Home view.

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,9 +1,9 @@
-module.exports = function(app, express){
- var apiRoutes = require('./api')(app, express);
- app.use('/api', apiRoutes);
+module.exports = function(app, express) {
+  var apiRoutes = require('./api')(app, express);
+  app.use('/api', apiRoutes);
 
   var path = require('path');
   app.get('*', function(req, res) {
-    res.sendFile(path.join(__dirname , '../../public/app/views/index.html'));
+    res.sendFile(path.join(__dirname, '../../public/app/views/index.html'));
   });
 };

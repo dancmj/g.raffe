676d8466267721f62ad9370a1a12f8f465cdf652
dancmj <daniel.c82@gmail.com>
2016-01-19 04:06:50 -0700

    WIP, progress with graffe directive, *New Functionalies:* Responsiveness added & it is now possible to add vertices and edges.

--- public/app/directives/graffe.directive.js
+++ public/app/directives/graffe.directive.js
@@ -12,75 +12,59 @@
       link: link,
       restrict: 'E',
       scope: {
-        data: '=graph'
+        graph: '='
       }
     };
     return directive;
 
     function link(scope, el, attrs) {
-        el = el[0];
-        var width;
-        var height;
-        var svg = d3.select(el).append("svg");
-        var fill = d3.scale.category10();
-        var nodes = d3.range(100).map(function(i) {
-          return {index: i};
-        });
-        var force = d3.layout.force();
+      el = el[0];
+      var width;
+      var height;
+      var graph = scope.graph;
+      var svg = d3.select(el).append("svg");
+      var nodes = graph.vertices;
+      var links = graph.edges;
+      var force = d3.layout.force();
+      var node = svg.selectAll(".node");
+      var link = svg.selectAll(".link");
 
-        scope.$watch(function(){
-          width = el.clientWidth;
-          height = el.clientHeight;
-          return width;
-        }, resize);
+      scope.$watch(function(){
+        start();
+        width = el.clientWidth;
+        height = el.clientHeight;
+        return width;
+      }, resize);
 
-        function resize() {
-          svg.attr({width: width, height: height - 5});
-          force.nodes(nodes).size([width, height])
-               .on("tick", tick)
-               .start();
-        }
+      function resize() {
+        svg.attr({width: width, height: height - 5});
+        force.nodes(nodes)
+             .links(links)
+             .size([width, height])
+             .on("tick", tick);
+      }
 
-        var node = svg.selectAll(".node")
-                      .data(nodes)
-                      .enter().append("circle")
-                      .attr("class", "node")
-                      .attr("cx", function(d) { return d.x; })
-                      .attr("cy", function(d) { return d.y; })
-                      .attr("r", 8)
-                      .style("fill", function(d, i) { return fill(i & 3); })
-                      .style("stroke", function(d, i) { return d3.rgb(fill(i & 3)).darker(2); })
-                      .call(force.drag)
-                      .on("mousedown", function() { d3.event.stopPropagation(); });
+      function start() {
+        link = link.data(force.links(), function(d) { return d.source.name + "-" + d.target.name; });
+        link.enter().insert("line", ".node").attr("class", "link");
+        console.log(link);
+        link.exit().remove();
 
-        svg.style("opacity", 1e-6)
-           .transition()
-           .duration(1000)
-           .style("opacity", 1);
+        node = node.data(force.nodes(), function(d) {return d.name;});
+        node.enter().append("circle").attr("class", function(d) { return "node " + d.name; }).attr("r", 8);
+        node.exit().remove();
 
-        d3.select("body")
-          .on("mousedown", mousedown);
+        force.start();
+      }
 
-        function tick(e) {
-
-          // Push different nodes in different directions for clustering.
-          var k = 6 * e.alpha;
-          nodes.forEach(function(o, i) {
-            o.y += i & 1 ? k : -k;
-            o.x += i & 2 ? k : -k;
-          });
-
-          node.attr("cx", function(d) { return d.x; })
-              .attr("cy", function(d) { return d.y; });
-        }
-
-        function mousedown() {
-          nodes.forEach(function(o, i) {
-            o.x += (Math.random() - .5) * 40;
-            o.y += (Math.random() - .5) * 40;
-          });
-          force.resume();
-        }
+      function tick() {
+        node.attr("cx", function(d) { return d.x; })
+            .attr("cy", function(d) { return d.y; });
+        link.attr("x1", function(d) { return d.source.x; })
+            .attr("y1", function(d) { return d.source.y; })
+            .attr("x2", function(d) { return d.target.x; })
+            .attr("y2", function(d) { return d.target.y; });
+      }
     }
   }
 })();
